0fcfd34962ac94cd35fe41bbccf190cfa5b5ebf9
Tau Jeng <taujeng.1@gmail.com>
2023-08-11 16:33:46 -0700

    Add card imgs

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <img src="images/header/logo.png" alt="PokerxAi logo" />
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Past Hands</li>
+                <li>More...</li>
+            </ul>
+        </nav>
+      
+    </header>
+  )
+}
+
+export default Header
